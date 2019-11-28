7c49256a77d34759aa4b5871c9c8aa07a501c05c
Paul Leger <pleger@gmail.com>
2019-11-28 13:56:03 +0900

    Examples, proceed support & mutual recursion avoided

---
+++ examples/example8.js
@@ -0,0 +1,7 @@
+let {Signal, SignalComp, Adaptation, CSI, show} = require("../loader");
+
+let temp = new Signal(0, "temp");
+let humidity = new Signal(0, "humid");
+
+
+
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //add to the array what example you want to execute it
-const EXAMPLE_SCRIPTS = [7];
+const EXAMPLE_SCRIPTS = [6];
 
 EXAMPLE_SCRIPTS.forEach(function (example) {
     console.log("\n\nEXECUTING THE EXAMPLE: " + example + "\n========================");
